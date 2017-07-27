1e8cdbab1e119faabce4bb254debba9b18470d89
Andrei Ababei <ababeiandrei94@gmail.com>
2017-07-27 21:34:51 +0300

    Create the project

---
+++ src/eveniment/Eveniment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eveniment;
+
+/**
+ *
+ * @author Andrei
+ */
+public class Eveniment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
